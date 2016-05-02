26b2d889afe75f49d92cef8cfbda282a6a833b92
mh9898 <mh9898@gmail.com>
2016-05-02 14:43:10 +0300

    Initial Commit

---
+++ PlayerVSEnemyTests/PlayerVSEnemyTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlayerVSEnemyTests.swift
+//  PlayerVSEnemyTests
+//
+//  Created by MiciH on 5/2/16.
+//  Copyright © 2016 MicahelH. All rights reserved.
+//
+
+import XCTest
+@testable import PlayerVSEnemy
+
+class PlayerVSEnemyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
